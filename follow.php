31eb0697bf0c51e7b51fe09d762660f0df068f60
AlexanderVinokurov <alexflight295@gmail.com>
2019-05-25 12:46:03 +0300

    Работает отписка от пользователей

--- follow.php
+++ follow.php
@@ -10,12 +10,10 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
-
-
-
 if (isset($_GET['user_id'])) {
     //Получаем id пользователя на которого будет осуществлена подписка
     $to_sub_id = $_GET['user_id'];
+
     //Проверяем есть пользователь с таким id
     $get_user_sql = "SELECT u.user_id FROM users u WHERE u.user_id = $to_sub_id";
     $get_user_res = mysqli_query($con,$get_user_sql);
@@ -32,7 +30,7 @@ if (isset($_GET['user_id'])) {
         if (isFollow($con,$to_sub_id)) {
             show_error('На этого пользователя вы уже подписаны');
         }
-        
+
         //Добавляем данные в таблицу
         $followers_add_sql = "INSERT INTO follow(who_sub_id, to_sub_id) VALUES (?,?)";
         $stmt = db_get_prepare_stmt($con,$followers_add_sql,[$who_sub_id,$to_sub_id]);
---
+++ unfollow.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('helpers.php');
+require_once('my_functions.php');
+require_once('sql_connect.php');
+
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: /");
+    exit();
+}
+
+if (isset($_GET['user_id'])) {
+    //Получаем id пользователя от которого нужно отписаться
+    $to_unsub_id = $_GET['user_id'];
+
+    //Проверяем есть пользователь с таким id
+    $get_user_sql = "SELECT u.user_id FROM users u WHERE u.user_id = $to_unsub_id";
+    $get_user_res = mysqli_query($con,$get_user_sql);
+    $get_user = mysqli_fetch_all($get_user_res, MYSQLI_ASSOC);
+
+    if (empty($get_user)) {
+        show_error('Пользователя с таким id не существует');
+    }
+    else {
+        //Получаем id пользователя который будет отписываться
+        $who_unsub_id = $_SESSION['user']['user_id'];
+
+        //Исключаем случай отписки от пользователя на которого вы не подписаны
+        if (!isFollow($con,$to_unsub_id)) {
+            show_error('Вы уже отписаны от данного пользователя');
+        }
+
+        //Удаляем данные из таблицы
+        $followers_delete_sql = "DELETE FROM follow WHERE who_sub_id = $who_unsub_id AND to_sub_id = $to_unsub_id";
+        $followers_delete_result = mysqli_query($con, $followers_delete_sql);
+
+        if ($followers_delete_result) {
+            $referer_url = $_SERVER['HTTP_REFERER'];
+            header("Location: $referer_url");
+            exit;
+        }
+        else {
+            $sql_error = mysqli_error($con);
+            show_error($sql_error);
+        }
+    }
+}
+else {
+    show_error('В параметрах GET запроса отсутствует id пользователя');
+}
